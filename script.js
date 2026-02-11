3b0c6f27e432eb72be0ea4e1deff29aa01252d94
José Vitor <josevitordesouza0@gmail.com>
2026-02-11 18:58:29 -0300

    mudança em geral

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// Navbar muda ao rolar
-window.addEventListener("scroll", function(){
-    const nav = document.getElementById("navbar");
-    nav.classList.toggle("scrolled", window.scrollY > 50);
-});
-
-// Animação dos cards
-const cards = document.querySelectorAll(".card");
-
-window.addEventListener("scroll", () => {
-    cards.forEach(card => {
-        const position = card.getBoundingClientRect().top;
-        const screenHeight = window.innerHeight;
-
-        if(position < screenHeight - 100){
-            card.classList.add("show");
-        }
-    });
+document.getElementById("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Estratégia enviada com sucesso! Entraremos em contato.");
 });
