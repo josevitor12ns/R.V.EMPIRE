ddb6f8adf3061c2e4d30c49b331fa4b52f76274c
José Vitor <josevitordesouza0@gmail.com>
2026-02-11 19:29:05 -0300

    geral

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// MENU MOBILE
 const toggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
@@ -7,4 +8,42 @@ toggle.addEventListener("click", function(){
     } else {
         menu.style.display = "flex";
     }
+});
+
+// ANIMAÇÃO AO ROLAR
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+    const reveals = document.querySelectorAll(".reveal");
+
+    for(let i=0;i<reveals.length;i++){
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if(elementTop < windowHeight - elementVisible){
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+// CONTADOR ANIMADO
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+    const updateCounter = () => {
+        const target = +counter.getAttribute("data-target");
+        const c = +counter.innerText;
+
+        const increment = target / 200;
+
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter,10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();
 });
